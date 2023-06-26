e6d2b1d1ab9ef8f1d9dfac2120127d0ee1e14777
Potat-OS1 <apotat.os1@gmail.com>
2023-06-26 01:44:02 -0700

    we figured out the 1d array thing woot!

--- src/main/java/com/example/test/ImageProcessingKernel.java
+++ src/main/java/com/example/test/ImageProcessingKernel.java
@@ -3,60 +3,59 @@ package com.example.test;
 import com.aparapi.Kernel;
 
 public class ImageProcessingKernel extends Kernel {
-    int[][][] base;
-    int[][][] overlay;
-    int[][][] result;
-    int x, y;
-    int maxX, maxY;
+    int[] base;
+    int baseWidth, baseHeight;
+    int[] overlay;
+    int overlayWidth, overlayHeight;
+    int a, b;
     int shadowLevel;
 
-//    ImageProcessingKernel (int[][][] base, int[][][] overlay, int x, int y) {
-//        this.base = base;
-//        this.overlay = overlay;
-//        maxX = overlay.length;
-//        maxY = overlay[0].length;
-//        result = base;
-//        this.x = x;
-//        this.y = y;
-//        shadowLevel = App.shadowLevel;
-//    }
-    ImageProcessingKernel (int shadowLevel) {
-        this.shadowLevel = shadowLevel;
-    }
-
-    public void setResult (int[][][] result) {
-        this.result = result;
-    }
-    public void assignWorkload (int[][][] base, int[][][] overlay, int x, int y) {
+    ImageProcessingKernel (int[] base, int baseWidth, int baseHeight) {
         this.base = base;
-        this.overlay = overlay;
-        maxX = overlay.length;
-        maxY = overlay[0].length;
-        result = base;
-        this.x = x;
-        this.y = y;
+        this.baseWidth = baseWidth;
+        this.baseHeight = baseHeight;
+        this.shadowLevel = App.shadowLevel;
     }
 
-    public int[][][] results () {
-        return result;
+    public int[] getImage () {
+        return base;
     }
+
+    public void addOverlay (int[] overlay, int overlayWidth, int overlayHeight, int xOffset, int yOffset) {
+        this.overlay = overlay;
+        this.overlayWidth = overlayWidth;
+        this.overlayHeight = overlayHeight;
+        a = xOffset;
+        b = yOffset;
+    }
+
+    public void reset (int[] base, int baseWidth, int baseHeight) {
+        this.base = base;
+        this.baseWidth = baseWidth;
+        this.baseHeight = baseHeight;
+    }
+
 
     @Override
     public void run() {
         int gid = getGlobalId();
-        int a = gid % maxX;
-        int b = gid / maxY;
+        int x = gid % overlayWidth;
+        int y = gid / overlayHeight;
 
         //x or y should not be more than the bounds of the screen.
-        if ((a + x) >= base.length || (b + y) >= base[0].length) {
+        if ((a + x) >= baseWidth || (b + y) >= baseHeight) {
             return;
         }
         if ((a + x) <= 0 || (b + y) <= 0) {
             return;
         }
-        result[a+x][b+y][0] = Math.min(Math.max(base[a+x][b+y][0], overlay[a][b][0]), shadowLevel);
-        result[a+x][b+y][1] = Math.max(base[a+x][b+y][1], overlay[a][b][1]);
-        result[a+x][b+y][2] = Math.max(base[a+x][b+y][2], overlay[a][b][2]);
-        result[a+x][b+y][3] = Math.max(base[a+x][b+y][3], overlay[a][b][3]);
+
+        int u = 4 * ((x+a) + ((y+b) * baseWidth));
+        int v = 4 * (x + (y * overlayWidth));
+
+        base[u] = Math.min(Math.max(base[u], overlay[v]), shadowLevel);
+        for (int z = 1; z < 4; z++) {
+            base[u+z] = Math.max(base[u+z], overlay[v+z]);
+        }
     }
 }
